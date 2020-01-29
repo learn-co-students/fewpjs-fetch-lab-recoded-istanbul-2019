0277b09ad10fb04588a1f0b9b9cc697461e388ca
zerozeynep <zeynep-demirtas@windowslive.com>
2020-01-30 01:09:29 +0300

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 function fetchBooks () {
-  return fetch('https://anapioficeandfire.com/api/books')
+   return fetch('https://anapioficeandfire.com/api/books')
   .then(resp => resp.json())
   .then(json => renderBooks(json))
-  
   }
 
 function renderBooks (json) {

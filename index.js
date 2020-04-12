0b1b4019530a56c69353929f8163dde4a89545ac
basil-kawak <basilalisa0@gmail.com>
2020-04-12 20:15:46 +0300

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 function fetchBooks() {
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(response => response.json())
+  .then(json => renderBooks(json));
+  // .then( function renderBooks(json) {
+  //   const main = document.querySelector('main')
+  //   json.forEach(book => {
+  //     const h2 = document.createElement('h2')
+  //     h2.innerHTML = `<h2>${book.name}</h2>`
+  //     main.appendChild(h2)
+  //   })
+    
+  // })
 
 }
+
 
 function renderBooks(json) {
   const main = document.querySelector('main')
@@ -9,7 +22,10 @@ function renderBooks(json) {
     h2.innerHTML = `<h2>${book.name}</h2>`
     main.appendChild(h2)
   })
+  
 }
+
+// renderBooks(json);
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()

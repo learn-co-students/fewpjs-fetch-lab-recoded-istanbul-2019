8630129274ae15f2fd9f7c00a5961c3838f6ce78
lilas-saleh <lilas.ahmad.saleh@gmail.com>
2020-04-14 10:27:13 +0300

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 function fetchBooks() {
-
-}
-
-function renderBooks(json) {
-  const main = document.querySelector('main')
-  json.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${book.name}</h2>`
-    main.appendChild(h2)
+  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(response => response.json())
+  .then(json => renderBooks(json))
+  }
+  
+  function renderBooks(json) {
+    const main = document.querySelector('main')
+    json.forEach(book => {
+      const h2 = document.createElement('h2')
+      h2.innerHTML = `<h2>${book.name}</h2>`
+      main.appendChild(h2)
+    })
+  }
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    fetchBooks()
   })
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
-})
